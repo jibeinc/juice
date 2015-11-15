4e5330836bfe93683585906e7d683ed79055741b
John Hatcher <jhatcher@jibe.com>
2015-11-15 16:33:48 -0500

    trying to set this up to work from Node

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// # FOR NPM USAGE
 'use strict';
 
-module.exports = require('./src/index.js');
+var env = require('jsdom').env;
+var html = '<html><body><h1>Hello World!</h1><p class="hello">Heya Big World!</body></html>';
+env(html, function (errors, window) {
+  console.error(errors);
+  var $ = require('jquery')(window);
+  module.exports = require('./src/index.js');
+});
