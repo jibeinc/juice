ffb40f5f45809085efdd4c3f41594059c5ef5c73
Robert Wagner <rwagner@jibe.com>
2015-11-20 16:23:17 -0500

    Fix expand/collapse, add in toggledClass and untoggledClass

--- src/ExpandCollapse/ExpandCollapseToggle/index.js
+++ src/ExpandCollapse/ExpandCollapseToggle/index.js
@@ -3,14 +3,30 @@
 const BaseComponent = require('../../BaseComponent');
 
 class ExpandCollapseToggle extends BaseComponent {
-  constructor(el) {
+  constructor(el, opts = {}) {
     super(el, {
       preserveChildElements: true
     });
     Object.assign(this, {
       value: null
     });
+    if (opts.untoggledClass) {
+      this.$el.addClass(opts.untoggledClass);
+    }
+    this.$el.addClass(opts.untoggledClass);
     this.$el.click(() => {
+      //If it is already toggled, we need to apply untoggled classes
+      if (opts.untoggledClass && this.value) {
+        if (opts.toggledClass) {
+          this.$el.removeClass(opts.toggledClass);
+        }
+        this.$el.addClass(opts.untoggledClass);
+      } else if (opts.toggledClass && !this.value) {
+        if (opts.untoggledClass) {
+          this.$el.removeClass(opts.untoggledClass);
+        }
+        this.$el.addClass(opts.toggledClass);
+      }
       this.set(!this.value);
     });
     return this;
