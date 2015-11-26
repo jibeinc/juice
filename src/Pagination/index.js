fc23401c618cefe09727fe9b4786865515f44752
Robert Wagner <rwagner@jibe.com>
2015-11-26 15:40:13 -0500

    Implement new pagination

--- src/Pagination/index.js
+++ src/Pagination/index.js
@@ -1,27 +1,24 @@
 'use strict';
 
 const BaseComponent = require('../BaseComponent');
-const pagination = require('pagination');
+const $ = require('jquery');
+const simplePagination = require('imports?jQuery=jquery!../../node_modules/simplePagination.js/jquery.simplePagination.js');
 const paginationTmpl = require('./pagination.dot');
 
 class Pagination extends BaseComponent {
   constructor(el, opts = {}) {
     super(el);
-    this.boostrapPaginator = new pagination.TemplatePaginator({
-      current: opts.current,
-      prelink: opts.prelink,
-      rowsPerPage: opts.rowsPerPage,
-      slashSeparator: opts.slashSeparator,
-      totalResult: opts.totalResult,
-      template(result) {
-        this.result = result;
-        return paginationTmpl(this);
-      }
+
+    this.$el.pagination({
+      cssStyle: opts.cssStyle || 'pagination',
+      edges: opts.edges || 0,
+      hrefTextPrefix: opts.hrefTextPrefix || '#page-',
+      items: 100,
+      itemsOnPage: opts.itemsOnPage || 10
     });
   }
 
   render() {
-    this.$el.html(this.boostrapPaginator.render());
     return this.$el.html();
   }
 }
