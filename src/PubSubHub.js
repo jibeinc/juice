3d29d2b1e11780b6495fd17e95a568c78614a443
John Hatcher <jhatcher@jibe.com>
2015-11-28 02:36:40 -0500

    run them in order of registration

--- src/PubSubHub.js
+++ src/PubSubHub.js
@@ -16,7 +16,7 @@ class PubSubHub {
     if (!this.listeners[namespace]) {
       this.listeners[namespace] = [];
     }
-    this.listeners[namespace].push(payload);
+    this.listeners[namespace].unshift(payload);
   }
 
   get(namespace) {
