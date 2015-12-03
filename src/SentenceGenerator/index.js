c5eec8c10c59b13f565f42e30d3e1087bc01a6ea
naveed <nnadjmabadi@jibe.com>
2015-12-03 10:56:47 -0500

    fix sum moar linting errors

--- src/SentenceGenerator/index.js
+++ src/SentenceGenerator/index.js
@@ -50,16 +50,16 @@ class SentenceGenerator extends BaseComponent {
     for (let i = 0; i < this.structure.length; i++) {
       
       // get the properties of each object
-      let segment  = this.structure[i];
-      let fragment = segment.fragment;
+      const segment  = this.structure[i];
       const fallback = segment.default;
       const required = segment.required;
+      let fragment = segment.fragment;
 
       // search and replace with the data values
       let matches;
       while ((matches = regex.exec(fragment)) !== null ) {
-        let matchStr  = matches[0];
-        let match     = matches[1];
+        const matchStr  = matches[0];
+        const match     = matches[1];
 
         if (dotty.exists(this, match)) {
           fragment = fragment.replace(matchStr, dotty.get(this,match));
@@ -89,7 +89,7 @@ class SentenceGenerator extends BaseComponent {
 
       // optional inclusion of the delimiter
       if (this.delimiter && (i < this.structure.length - 1)) {
-       templateString += this.delimiter; 
+        templateString += this.delimiter; 
       }
     }
 
