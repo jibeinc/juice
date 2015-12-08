cc0deca94243388ba52f222738aac291e6a22ec5
naveed <nnadjmabadi@jibe.com>
2015-12-08 15:29:18 -0500

    Update single select to support display values

--- src/SingleSelect/index.js
+++ src/SingleSelect/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // # TODO
-//    - support separation of value from displayValue
 //    - styles
 
 // css
@@ -18,6 +17,13 @@ class SingleSelect extends BaseComponent {
   constructor(el, opts={}) {
     super(el);
     this.options = (opts.options || []).map((opt) => {
+      if ($.isPlainObject(opt)) {
+        return {
+          display: opt.display,
+          value: opt.value,
+          selected: opt.value === this.get()
+        };
+      }
       return {
         value: opt,
         selected: opt === this.get()
@@ -27,8 +33,14 @@ class SingleSelect extends BaseComponent {
 
   set(v) {
     this.options = this.options.map((opt) => {
-      opt.selected = opt.value === v;
-      return opt;
+      if (opt.display) {
+        opt.selected = opt.display === v;
+        return opt;
+      }
+      else {
+        opt.selected = opt.value === v;
+        return opt;
+      }
     });
     return super.set(v);
   }
