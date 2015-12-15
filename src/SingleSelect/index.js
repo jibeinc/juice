d89247729ec0c0b033df334acf44d2c3e64a67e0
naveed <nnadjmabadi@jibe.com>
2015-12-15 12:14:41 -0500

    Add getDisplayValue() method to singleSelect

--- src/SingleSelect/index.js
+++ src/SingleSelect/index.js
@@ -31,6 +31,21 @@ class SingleSelect extends BaseComponent {
     });
   }
 
+// @method
+// @returns the display attribute of the option if it exists, fallback to the value
+  getDisplayValue() {
+    let display;
+
+    for (const opt of this.options) {
+      if (opt.selected) {
+        display = (opt.display) ? opt.display : opt.value;
+      }
+    }
+    return display;
+  }
+
+// @method
+// @param {Object} v - the Option Object (display,value,selected) to set
   set(v) {
     this.options = this.options.map((opt) => {
       if (opt.display) {
