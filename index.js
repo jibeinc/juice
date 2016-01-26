4392cdad234f3dda2f475bc62f2329a65be4e14b
Robert Wagner <rwagner@jibe.com>
2016-01-26 13:01:44 -0500

    Add babel polyfill

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // # FOR NPM USAGE
 'use strict';
 
-require('babel-polyfill');
-
 module.exports = function (done) {
   var env = require('jsdom').env;
   var html = '<html><body><h1>Hello World!</h1><p class="hello">Heya Big World!</body></html>';
-  env(html, ['https://cdnjs.cloudflare.com/ajax/libs/dot/1.0.3/doT.min.js', __dirname + '/dist/ui.js'], function (errors, window) {
-    if (errors) {
-      console.error('failed to compile jibe-ui-components for node', errors);
-    }
+  env(html, ['https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.3.14/polyfill.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/dot/1.0.3/doT.min.js', __dirname + '/dist/ui.js'],
+    function (errors, window) {
+      if (errors) {
+        console.error('failed to compile jibe-ui-components for node', errors);
+      }
 
-    global.window = window;
-    global.document = window.document;
-    done(window.UI);
-  });
+      global.window = window;
+      global.document = window.document;
+      done(window.UI);
+    });
 };
 
 // module.exports = require('./src/index');
