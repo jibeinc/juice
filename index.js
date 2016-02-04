0e253e847969281bd656615dd374732f726c7e24
John Hatcher <jhatcher@jibe.com>
2016-02-04 12:23:21 -0500

    pulling out dependencies broke everything for node

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ require('babel-polyfill');
 module.exports = function (done) {
   var env = require('jsdom').env;
   var html = '<html><body><h1>Hello World!</h1><p class="hello">Heya Big World!</body></html>';
-  env(html, ['https://cdnjs.cloudflare.com/ajax/libs/dot/1.0.3/doT.min.js', __dirname + '/dist/ui.js'], function (errors, window) {
+  env(html, [
+    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/dot/1.0.3/doT.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.2.1/lodash.min.js',
+    __dirname + '/dist/ui.js'
+  ], function (errors, window) {
     if (errors) {
       console.error('failed to compile jibe-ui-components for node', errors);
     }
