9491199d2f5e794a2e71e5e36ecb80465d0d132f
Travis CI <rwwagner90@gmail.com>
2016-04-18 15:44:22 -0400

    Go back to deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,6 @@ mkdir dist;
 # run our compile script, discussed above
 npm run build
 
-# inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "rwwagner90@gmail.com"
-
 git add --force dist/
 # bump bower.json and package.json
 mversion patch -m 'Bumped to %s'
-
-git push "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
