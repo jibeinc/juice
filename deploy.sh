df95f5fa6964cc912ace65f50914fa2e2f2d8a74
Travis CI <rwwagner90@gmail.com>
2016-04-20 17:28:54 -0400

    Never going to work

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 set -e # exit with nonzero exit code if anything fails
 
 # inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI";
-git config user.email "jjt@jibe.com";
+git config user.name "Travis CI"
+git config user.email "jjt@jibe.com"
 
-git checkout master;
-git pull;
+git checkout master
+git pull
 
 eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key.pem
@@ -14,19 +14,17 @@ ssh-add .travis/deploy_key.pem
 git remote set-url origin git@github.com:jibeinc/juice.git
 
 # clear and re-create the dist directory
-rm -rf dist || exit 0;
-mkdir dist;
+rm -rf dist || exit 0
+mkdir dist
 
 # run our compile script, discussed above
-npm run build;
+npm run build
 
-git add --force dist/;
-git commit -m "New dist generated [ci skip]";
-echo "Push new dist";
-git push --quiet;
-
+git add --force dist/
 # bump bower.json and package.json
-echo "Bump version";
-mversion patch -m "Bumped version to %s";
-echo "Push new tags";
-git push --quiet --tags;
+mversion patch
+git add .
+git commit -m "New dist generated [ci skip]"
+
+echo "Push new dist"
+git push --quiet
