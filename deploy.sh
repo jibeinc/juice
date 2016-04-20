4a385cc16dbed8e0e931741d85ac316bb6271e27
Travis CI <rwwagner90@gmail.com>
2016-04-20 11:00:47 -0400

    More stuff

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "rwwagner90@gmail.com"
+
+git remote add origin https://github.com/jibeinc/juice.git
+git checkout origin/master
+
 # clear and re-create the dist directory
 rm -rf dist || exit 0;
 mkdir dist;
 
 # run our compile script, discussed above
 npm run build
-
-# inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "rwwagner90@gmail.com"
-
-git checkout https://${GH_USER}:${GH_PASSWORD}@${GH_REF}
 
 git add --force dist/
 git commit -m "New dist generated"
@@ -24,4 +25,4 @@ git status
 
 # We redirect any output to /dev/null to hide any sensitive credential
 # data that might otherwise be exposed.
-git push --quiet "https://${GH_USER}:${GH_PASSWORD}@${GH_REF}" > /dev/null 2>&1
+git push --quiet "https://${GH_USER}:${GH_PASSWORD}@${GH_REF}" master > /dev/null 2>&1
