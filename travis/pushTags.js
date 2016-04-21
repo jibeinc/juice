04af2279b271dceeb93935475d1399ddbbad647c
Travis CI <rwwagner90@gmail.com>
2016-04-21 13:35:19 -0400

    Fix spaces

--- travis/pushTags.js
+++ travis/pushTags.js
@@ -2,5 +2,5 @@ require('shelljs/global');
 var version = 'v' + require('/package.json').version;
 console.log('Package version:', version);
 echo('Echo version: ' + version);
-exec('git tag -a' + version  + '-m "Release' + version + '";');
+exec('git tag -a ' + version  + ' -m "Release ' + version + '";');
 exec('git push --quiet --tags;');
