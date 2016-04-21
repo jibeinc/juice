d58405d8e8f5e1c952168fec92de662376257bd9
Travis CI <rwwagner90@gmail.com>
2016-04-21 14:20:15 -0400

    Remove logs, try to build tag branch

--- travis/pushTags.js
+++ travis/pushTags.js
@@ -1,5 +1,5 @@
 require('shelljs/global');
 var version = 'v' + require('../package.json').version;
 console.log('Package version:', version);
-console.log(exec('git tag -a ' + version  + ' -m "Release ' + version + '";'));
-console.log(exec('git push --quiet --tags;'));
+exec('git tag -a ' + version  + ' -m "Release ' + version + '";');
+exec('git push --quiet --tags;');
