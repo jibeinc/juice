a30c93af158a75f1703ec88db5eade8c47c61d7a
Travis CI <rwwagner90@gmail.com>
2016-04-21 13:34:39 -0400

    Log version

--- travis/pushTags.js
+++ travis/pushTags.js
@@ -1,4 +1,6 @@
 require('shelljs/global');
-var version = 'v' + require('../package.json').version;
+var version = 'v' + require('/package.json').version;
+console.log('Package version:', version);
+echo('Echo version: ' + version);
 exec('git tag -a' + version  + '-m "Release' + version + '";');
 exec('git push --quiet --tags;');
