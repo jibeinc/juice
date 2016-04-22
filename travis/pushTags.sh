2ed86d27c60b9569684a03466a48bc691b0f0de6
Travis CI <rwwagner90@gmail.com>
2016-04-22 12:15:26 -0400

    Add back commit

--- travis/pushTags.sh
+++ travis/pushTags.sh
@@ -4,6 +4,7 @@ mversion patch
 git add .
 VERSION="$(grep version package.json | head -1 | awk -F: '{print $2}' | sed 's/[",]//g' | tr -d '[:space:]')"
 echo $VERSION
-git tag -a "v${VERSION}" -m "Release v${VERSION} [ci skip]"
+git commit -m "Build v${VERSION} [ci skip]"
+git tag -a "v${VERSION}" -m "Release v${VERSION}"
 git tag
 git push --follow-tags
