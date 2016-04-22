bf927abf24593fd2f9326094d727e8cd09833fbd
Travis CI <rwwagner90@gmail.com>
2016-04-22 11:45:13 -0400

    Move to script

---
+++ travis/pushTags.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git add --force dist/;
+mversion patch;
+git add .;
+VERSION="$(grep version package.json | head -1 | awk -F: '{print $2}' | sed 's/[",]//g' | tr -d '[:space:]')";
+echo $VERSION;
+git tag -a "v${VERSION}" -m "Release v${VERSION} [ci skip]";
+git tag;
+git push --follow-tags;
