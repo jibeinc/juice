a0700785472d363036df32b13092cf00fc3fa13f
Travis CI <rwwagner90@gmail.com>
2016-04-22 10:43:10 -0400

    Trying one push

--- travis/pushTags.js
+++ travis/pushTags.js
@@ -1,6 +1,6 @@
 require('shelljs/global');
 var version = 'v' + require('../package.json').version;
 console.log('Package version:', version);
-exec('git tag -a ' + version  + ' -m "Release ' + version + '";');
+exec('git tag -a ' + version  + ' -m "Release ' + version + '[ci skip]";');
 exec('git tag');
-exec('git push origin --tags;');
+exec('git push --follow-tags;');
