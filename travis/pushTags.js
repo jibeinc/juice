7bc0d6b87cfd1337c7605d70f3a6cd065d1dd679
Travis CI <rwwagner90@gmail.com>
2016-04-22 10:27:13 -0400

    Remove quiet

--- travis/pushTags.js
+++ travis/pushTags.js
@@ -2,4 +2,5 @@ require('shelljs/global');
 var version = 'v' + require('../package.json').version;
 console.log('Package version:', version);
 exec('git tag -a ' + version  + ' -m "Release ' + version + '";');
-exec('git push --quiet && git push --tags --quiet;');
+exec('git tag');
+exec('git push origin --tags;');
