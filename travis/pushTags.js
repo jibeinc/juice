6eb9c59db6b392556e867079743934fe3a788d77
Travis CI <rwwagner90@gmail.com>
2016-04-22 13:16:29 -0400

    Remove need for js hopefully

--- travis/pushTags.js
+++ travis/pushTags.js
@@ -1,9 +1,6 @@
 require('shelljs/global');
 var version = 'v' + require('../package.json').version;
 console.log('Package version:', version);
-exec('git add --force dist/');
-exec('mversion patch');
-exec('git add .');
 exec('git commit -m "Build ' + version + ' [skip build]"');
 exec('git tag -a ' + version  + ' -m "Release ' + version + '";');
 exec('git tag');
