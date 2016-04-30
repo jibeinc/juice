125228b4f0b06f629e255f9ee4720dbfcf0bb1b0
Robert Wagner <rwwagner@vt.edu>
2016-04-30 16:21:26 -0400

    Fix pagination tests

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,3 +1,9 @@
+const $ = require('cash-dom');
+window.$ = $;
+window.jQuery = $;
+window.jquery = $;
+require('imports?jQuery=cash-dom!./vendor/jquery.simplePagination.js');
+
 // Tests
 const testsContext = require.context('./tests', true, /-test\.js$/);
 testsContext.keys().forEach(testsContext);
--- tests/pagination-test.js
+++ tests/pagination-test.js
@@ -1,56 +1,56 @@
-// const Pagination = require('../src/Pagination/index.js');
-//
-// describe('pagination functionality', () => {
-//   let pagination;
-//   const liSelector = '.pagination-test.pagination li';
-//   let testOnPageClickCalled = false;
-//
-//   beforeEach(() => {
-//     $('body').append('<div class="pagination-test"></div>');
-//     pagination = new Pagination('.pagination-test', {
-//       cssStyle: 'pagination',
-//       edges: 0,
-//       hrefTextPrefix: '#?page=',
-//       itemsOnPage: 10,
-//       nextText: '<span class="icon icon-arrow_forward icon-arrow"></span>',
-//       onPageClick() {
-//         testOnPageClickCalled = true;
-//       },
-//       prevText: '<span class="icon icon-arrow_back icon-arrow"></span>'
-//     });
-//     pagination.render();
-//   });
-//
-//   afterEach(() => {
-//     $('body').empty();
-//   });
-//
-//   it('test onPageClick is called', () => {
-//     expect(testOnPageClickCalled).toBe(false);
-//     $($(liSelector)[2]).find('a').trigger('click');
-//     expect(testOnPageClickCalled).toBe(true);
-//   });
-//
-//   it('test next/previous', () => {
-//     $($(liSelector)[6]).find('a').trigger('click');
-//     expect(pagination.get()).toBe(2);
-//     $($(liSelector)[0]).find('a').trigger('click');
-//     expect(pagination.get()).toBe(1);
-//   });
-//   it('test clicking page number', () => {
-//     $('body').append('<div class="pagination-test"></div>');
-//     pagination = new Pagination('.pagination-test', {
-//       cssStyle: 'pagination',
-//       edges: 0,
-//       hrefTextPrefix: '#?page=',
-//       itemsOnPage: 10,
-//       nextText: '<span class="icon icon-arrow_forward icon-arrow"></span>',
-//       prevText: '<span class="icon icon-arrow_back icon-arrow"></span>'
-//     });
-//     pagination.render();
-//
-//     expect(pagination.get()).toBe(1);
-//     $($(liSelector)[4]).find('a').trigger('click');
-//     expect(pagination.get()).toBe(4);
-//   });
-// });
+const Pagination = require('../src/Pagination/index.js');
+
+describe('pagination functionality', () => {
+  let pagination;
+  const liSelector = '.pagination-test.pagination li';
+  let testOnPageClickCalled = false;
+
+  beforeEach(() => {
+    $('body').append('<div class="pagination-test"></div>');
+    pagination = new Pagination('.pagination-test', {
+      cssStyle: 'pagination',
+      edges: 0,
+      hrefTextPrefix: '#?page=',
+      itemsOnPage: 10,
+      nextText: '<span class="icon icon-arrow_forward icon-arrow"></span>',
+      onPageClick() {
+        testOnPageClickCalled = true;
+      },
+      prevText: '<span class="icon icon-arrow_back icon-arrow"></span>'
+    });
+    pagination.render();
+  });
+
+  afterEach(() => {
+    $('body').empty();
+  });
+
+  it('test onPageClick is called', () => {
+    expect(testOnPageClickCalled).toBe(false);
+    $($(liSelector)[2]).find('a').trigger('click');
+    expect(testOnPageClickCalled).toBe(true);
+  });
+
+  it('test next/previous', () => {
+    $($(liSelector)[6]).find('a').trigger('click');
+    expect(pagination.get()).toBe(2);
+    $($(liSelector)[0]).find('a').trigger('click');
+    expect(pagination.get()).toBe(1);
+  });
+  it('test clicking page number', () => {
+    $('body').append('<div class="pagination-test"></div>');
+    pagination = new Pagination('.pagination-test', {
+      cssStyle: 'pagination',
+      edges: 0,
+      hrefTextPrefix: '#?page=',
+      itemsOnPage: 10,
+      nextText: '<span class="icon icon-arrow_forward icon-arrow"></span>',
+      prevText: '<span class="icon icon-arrow_back icon-arrow"></span>'
+    });
+    pagination.render();
+
+    expect(pagination.get()).toBe(1);
+    $($(liSelector)[4]).find('a').trigger('click');
+    expect(pagination.get()).toBe(4);
+  });
+});
