a969137ed926b6cf9707066d1bcbb6e211717a63
naveed <naveedn@terpmail.umd.edu>
2016-04-30 14:48:07 -0400

    Add tests for assert

---
+++ src/assert-test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('./assert.js');
+
+describe('Assert function', () => {
+  it('should throw error when condition isn\'t met', () => {
+    expect(() => {
+      assert(typeof 3 === 'string');
+    }).toThrow(new Error('Assertion failure'));
+  });
+
+  it('should throw error with specific message', () => {
+    expect(() => {
+      assert(typeof 3 === 'string', 'custom error message');
+    }).toThrow(new Error('custom error message'));
+  });
+
+  it('should do nothing when condition is met', () => {
+    expect(() => {
+      assert(typeof 3 === 'number');
+    }).not.toThrow();
+  });
+});
