7b4f3cf0ad39e3338531818c7f3ef44da345ad72
Travis CI <rwwagner90@gmail.com>
2016-06-02 16:31:17 -0400

    Add close button

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -12,12 +12,16 @@ class Modal extends BaseComponent {
    * @param {string} el - The selector for the element to put the Modal component in
    * @param {string} content - The markup of the content to put inside the modal
    * @param {object} opts - The options for the component
+   * @param {string} [opts.closeButton] - The markup to render for the close button
+   * @param {boolean} [opts.hideOnOverlayClick] - A boolean indicating if clicking the overlay should close the modal
+   * @param {boolean} [opts.showOverlay] - A boolean to show the grey overlay or not
    */
   constructor(el, content, opts = {}) {
     if (typeof content === 'string') {
       super(el, {preserveChildElements: true});
       Object.assign(this, {
         content,
+        closeButton: opts.closeButton || 'X',
         hideOnOverlayClick: opts.hideOnOverlayClick || false,
         showOverlay: opts.showOverlay || false
       });
@@ -43,11 +47,19 @@ class Modal extends BaseComponent {
    */
   render() {
     this.$el.prepend(modalTmpl(this));
+
+    // Close on overlay click
     if (this.hideOnOverlayClick) {
       this.$el.find('.ui-modal-overlay').on('click', () => {
         this.hide();
       });
     }
+
+    // Close on "X" click
+    this.$el.find('.ui-modal-content .close-button').on('click', () => {
+      this.hide();
+    });
+
     return this.$el.html();
   }
 
